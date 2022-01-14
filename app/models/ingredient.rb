a7f898dc2500ae3920fa40ff85407e95165fe26b
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-14 14:42:29 -0500

    created associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+    has_many :ingredients_recipes
+    has_many :recipes, through: :ingredients_recipes
+
+    validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    has_many :ingredients_recipes
+    has_many :ingredients, through: :ingredients_recipes
+
+    belongs_to :user
+    belongs_to :weeks
+
+    validates :name, presence: true
 end
